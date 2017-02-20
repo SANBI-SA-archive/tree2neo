6fba07ca1b6639a7be2b62294b79298514c01e25
Zipho Mashologu <zipho.m@gmail.com>
2017-02-20 13:36:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,13 @@ setup(
     description='Parses FastTree file and load it to the graph database.',
     keywords='neo4j,and fasttree',
     py_modules=['tree2neo'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'click',
         'bioservices',
-        'py2neo'
+        'py2neo',
+        'tqdm'
     ],
     entry_points={
         'console_scripts': ['tree2neo=tree2neo.cli:cli']
